05e9e2384dbdee03bce0207128e019f50bd22c63
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-08-11 16:12:15 +0200

    Add event emitter

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 //importowanie modułu
 var os = require('os');
 
+//import klasy EventEmitter z modułu event
+var EventEmitter = require('events').EventEmitter;
+
 //korzystanie z modułu
 var OSinfo = require('./modules/OSinfo');
+
+//utworzenie obiektu na podstawie klasy
+var emitter = new EventEmitter();
+
+//nasłuchiwania zdarzeń
+emitter.on('beforeCommand', function(instruction) {
+    console.log('You wrote: ' + instruction + ' trying to run command.')
+});
+emitter.on('afterComand', function() {
+    console.log('Finished command');
+});
+
 //odczytywanie wartości z kodowaniem UTF-8
 process.stdin.setEncoding('utf-8');
 //nasłuchiwanie na zdarzenie odczytu
@@ -11,6 +26,8 @@ process.stdin.on('readable', function() {
     if (input !== null) {
         // trim() służy do pozbywania się białych znaków z przodu i za tekstem
         var instruction = input.trim();
+        //odpalenie zdarzenia beforeCommand
+        emitter.emit('beforeCommand', instruction)
         switch(instruction) {
             case '/exit':
                 process.stdout.write('Qutting app!\n');
@@ -27,6 +44,9 @@ process.stdin.on('readable', function() {
             default:
                 process.stderr.write('Wrong instruction!\n');    
         };
+        //odpalenie zdarzenia afterCommand
+        emitter.emit('afterCommand')
+
     }
 })
 
